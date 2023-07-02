d0904895561114166ca7bdcf56de2b33ab78ddf8
kirill.sharov <kirill.scharov@gmail.com>
2023-07-02 18:30:15 +0300

    Added a solution to Task004

---
+++ HW_1/Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите последовательно три числа. После ввода каждого числа нажмите клавишу 'ENTER'");
+int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+int thirdNum = int.Parse(Console.ReadLine());
+
+int maxNum = firstNum;
+
+if (secondNum > maxNum)
+{
+     maxNum = secondNum;
+}
+    if (thirdNum > maxNum)
+    {
+        maxNum = thirdNum;
+    }
+
+Console.WriteLine($"Из чисел: {firstNum}, {secondNum}, {thirdNum} максимальное - {maxNum}");
+
