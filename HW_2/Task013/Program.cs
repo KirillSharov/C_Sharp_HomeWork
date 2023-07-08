a6350a2aaf35ab29b5a1ecd9192a20579520ed6d
kirill.sharov <kirill.scharov@gmail.com>
2023-07-08 16:28:44 +0300

    Added a solution to Task13

--- HW_2/Task013/Program.cs
+++ HW_2/Task013/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите любое число: ");
+string num = Console.ReadLine();
+char[] array = num.ToCharArray();
+
+if (array.Length < 3)
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+else
+{
+Console.WriteLine($"{num} -> {array[2]}");
+}
+
+
