c6b2c01733d59b23dcad8a95c7605b38fbc01285
kirill.sharov <kirill.scharov@gmail.com>
2023-07-09 17:01:18 +0300

    Added Task19 and changed Readme

---
+++ HW_3/Task019/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string Palindrome(string num)
+{
+    char[] arrayNum = num.ToCharArray();
+    if (arrayNum[0] == arrayNum[4] & arrayNum[1] == arrayNum[3]) return "да";
+    else return "нет";
+}
+
+string GetInfo (string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+string num = GetInfo("Введите пятизначное число: ");
+Console.WriteLine($"{num} -> {Palindrome(num)}");
