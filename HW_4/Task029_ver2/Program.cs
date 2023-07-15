3bf5e5847e07cc282d8e20dafe47d8c58cbb24fd
kirill.sharov <kirill.scharov@gmail.com>
2023-07-16 01:06:26 +0300

    Added a second solution to Task29

---
+++ HW_4/Task029_ver2/Program.cs
@@ -0,0 +1,25 @@
+﻿void PrintArray(int i, int[] array)
+{
+    Console.Write("[");
+    for (int k = 0; k < i-1; k++)
+    {
+        Console.Write($"{array[k]}, ");
+    }
+    Console.Write($"{array[i - 1]}]");
+}
+
+int FillArray(string message, int[] array)
+{
+    string userElement;
+    int i = 0;
+    Console.WriteLine(message);
+    while ((userElement = Console.ReadLine()) != "end")
+    {
+        array[i++] = Convert.ToInt32(userElement);
+    }
+    return i;
+}
+
+int[] userArray = new int[100];
+int i = FillArray("Введите последовательность чисел для заполнения массива, когда закончите - введите 'end'", userArray);
+PrintArray(i, userArray);
