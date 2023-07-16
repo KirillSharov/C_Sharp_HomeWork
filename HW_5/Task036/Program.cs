a0cb58c72181e59926355eedd513d9f7b6d51b64
kirill.sharov <kirill.scharov@gmail.com>
2023-07-16 20:49:07 +0300

    Added a solution to Task36

--- HW_5/Task036/Program.cs
+++ HW_5/Task036/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int SumOddElements(int[] array)
+{
+    int oddSum = 0;
+    for(int i = 1;  i < array.Length; i+=2)
+    {
+           oddSum += array[i]; 
+    }
+    return oddSum;
+}
+
+int[] MakeNewRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int[] array = MakeNewRandomArray(15, -100, 100);
+Console.WriteLine($"[{string.Join(",", array)}] -> {SumOddElements(array)}");
